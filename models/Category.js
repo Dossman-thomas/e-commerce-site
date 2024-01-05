91e9d689a43644f76ed7deafb5144a5cc86d2ea2
Tom Dossman <tdossman.development@gmail.com>
2024-01-05 17:55:51 -0500

    product model column values added

--- models/Category.js
+++ models/Category.js
@@ -10,11 +10,11 @@ Category.init(
       type: DataTypes.INTEGER,
       allowNull: false, 
       primaryKey: true,
-      autoIncrement: true,
+      autoIncrement: true
     },
     category_name: {
       type: DataTypes.INTEGER,
-      allowNull: false,
+      allowNull: false
     }
   },
   {
